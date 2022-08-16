13b996170eb04adae62ac25ed5aa6768499df83f
Vítor Cardoso Bertolucci <vitorbertolucci@gmail.com>
2022-08-16 15:24:11 -0300

    Organize exports

---
+++ src/errors/index.ts
@@ -0,0 +1,1 @@
+export { default as TimeoutError } from './TimeoutError';
---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+export { sha256, sha512 } from './cryptography';
+export { default as getIpFromRequest } from './getIpFromRequest';
+export { default as getRandomArrayElement } from './getRandomArrayElement';
+export { default as getRandomInteger } from './getRandomInteger';
+export { default as httpFetch } from './httpFetch';
+export { default as passRandomChance } from './passRandomChance';
+export { default as round } from './round';
+export { default as sleep } from './sleep';
